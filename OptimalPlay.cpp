f3b3aebeab5efa1742c2da18f2c2e0779f3ae08c
EmreDincoglu <Dincoglue@gmail.com>
2025-03-22 21:59:48 -0400

    OptimalPlay and dealer and edited some code
    Added optimalplay and the dealer class, OptimalPlay tells you what moves to do when, dealer called once will do its required steps until its done

---
+++ OptimalPlay.cpp
@@ -0,0 +1,31 @@
+#include "OptimalPlay.h"
+#include "Hand.h"
+using namespace std;
+
+char OptimalPlay::getMove(Hand& hand, Card dealerShowing) const {
+	int totalHand = hand.getTotalValue();
+	bool isSoft = hand.isSoft();
+	int dealerCard = static_cast<int> (dealerShowing.getRank());
+	if (dealerCard > 10 && dealerCard < 14) dealerCard = 10;
+	if (dealerCard == 14) dealerCard = 11;
+
+	if (hand.isPair()) {
+		return SplitPair[(totalHand / 2) - 2][dealerCard - 2];
+	}
+	if (hand.isSoft()) {
+		return SoftTotal[totalHand - 13][dealerCard - 2];
+	}
+	return HardTotal[totalHand - 4][dealerCard - 2];
+
+}
+//sleuthMode means if you are trying to avoid casino detection, making higher and higher bets is good
+//but if you bet too high when your at an advantage then the casino gets suspicious
+//sleuth mode true means capped bets, false means uncapped bet
+int OptimalPlay::getBet(int runningCount, int decksRemaining, int bankroll, bool sleuthMode) const {
+	int trueCount = runningCount / decksRemaining;
+	int bettingUnit = bankroll / 1000;
+	int totalBetSize = bettingUnit * (trueCount - 1);
+	if (totalBetSize < 5) totalBetSize = 5;
+	if (totalBetSize > bankroll / 4 && sleuthMode) totalBetSize = bankroll / 4;
+	return totalBetSize;
+}
